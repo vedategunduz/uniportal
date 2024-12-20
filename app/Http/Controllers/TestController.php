f521f4c6079fccc64a48bf8132a24c5ffdb28d07
vedat <vedat.bilisim@outlook.com>
2024-12-20 11:42:10 +0300

    silinen bos sayfalar

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,22 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Firma;
-use App\Models\Kamu;
-
 class TestController extends Controller
 {
-    protected $veriler;
 
-    public function firmalar() {
-        $this->veriler = Firma::all();
-
-        return view('home', ['veriler' => $this->veriler]);
-    }
-
-    public function kamular() {
-        $this->veriler = Kamu::all();
-
-        return view('home', ['veriler' => $this->veriler]);
-    }
 }
